02790d1beedf5d5cae0a7ca77798ac5f08d0bd18
Igor Fedonenkov <fedaligo@mail.ru>
2020-01-04 14:46:39 +0300

    feature-6

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -1,2 +1,22 @@
+import java.util.Objects;
+
 public class House {
+    int x;
+    int y;
+    int z;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        House house = (House) o;
+        return x == house.x &&
+                y == house.y &&
+                z == house.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
-public class Main {
+class Q implements Runnable {
+    int i = 0;
+
+    public int run() {
+        System.out.println("i=" + ++i);
+        return i;
+    }
+}
+    public class Main {
+    }
     public static void main(String[] args) {
         System.out.println("sdfsdf");
     }
