ad10f5bc14002a8df008ad0da8538572d6776638
Igor Fedonenkov <fedaligo@mail.ru>
2020-01-04 16:03:18 +0300

    feature-7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-class Q implements Runnable {
-    int i = 0;
+public class Main extends Thread {
 
-    public int run() {
-        System.out.println("i=" + ++i);
-        return i;
-    }
-}
-    public class Main {
-    }
     public static void main(String[] args) {
-        System.out.println("sdfsdf");
+
+        new Main().start();
+        System.out.println("111");
     }
 }
